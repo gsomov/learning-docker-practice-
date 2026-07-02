48e6c8ce2a1346257224ddcc9c9ae5af6c03a2ef
S1mka <goshasomov53@gmail.com>
2026-07-03 00:31:57 +0300

    Docker окружение для проекта
    Добавлены Dockerfile, docker-compose.yml, .dockerignore.
    
    Настроен Hot Reload внутри контейнера через host:true и usePolling в vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true,
+//     port: 5173,
+//   },
+// })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
+})
